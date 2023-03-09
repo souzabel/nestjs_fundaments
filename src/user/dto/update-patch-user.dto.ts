e94ff08fbc0a6f113fec58834ee9ee07553bd874
Isabel Pereira de Souza <contatobelsouza@gmail.com>
2023-03-09 14:50:12 -0300

    prisma

--- src/user/dto/update-patch-user.dto.ts
+++ src/user/dto/update-patch-user.dto.ts
@@ -1,6 +1,6 @@
 import { CreateUserDTO } from "./create-user.dto";
 import { PartialType } from "@nestjs/mapped-types";
 
-export class UpdatePutUserDTO extends PartialType (CreateUserDTO) {
+export class UpdatePatchUserDTO extends PartialType (CreateUserDTO) {
 
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body, Get, Param, Put, Patch, Delete} from "@nestjs/common/decorators";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+import { ParseIntPipe } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
@@ -16,32 +18,32 @@ export class UserController {
     }
 
     @Get(':id')
-    async show(@Param() param){
-        return {user: {}, param}
+    async show(@Param('id', ParseIntPipe) id: number){
+        return {user: {}, id}
     }
 
     @Put(':id')
-    async update(@Body() {name, email, password}: UpdatePutUserDTO, @Param() params){
+    async update(@Body() {name, email, password}: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number){
         return {
             method: 'put',
             name, email, password,
-            params
+            id
         }
     }
 
     @Patch(':id')
-    async updatePartial(@Body() body, @Param() params){
+    async updatePartial(@Body() {name, email, password}: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number){
         return {
             method: 'put',
-            body,
-            params
+            name, email, password,
+            id
         }
     }
 
     @Delete(':id')
-    async delete(@Param() params){
+    async delete(@Param('id', ParseIntPipe) id: number){
         return {
-            params
+            id
         }
     }
 }
